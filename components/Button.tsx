5d4b681ebeecd6ff0229c0404919b6ff7dd5caaf
Mfundo Shabalala <mfundoshabalala@gmail.com>
2022-04-30 08:53:04 +0200

    Button component update

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,6 +2,7 @@ type ButtonInterface = React.FunctionComponent<{
 	name: string;
 	type?: 'button' | 'submit';
 	color?: string | 'primary' | 'danger' | 'disabled' | 'caution';
+	onClick?: (() => Promise<void>) | ((event: React.FormEvent) => void);
 }>;
 
 const Button: ButtonInterface = ({ name, type = 'button', color = 'primary' }) => {
