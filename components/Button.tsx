907d4ffdf903f2f46915892d6d0fec72fb8964a6
Mfundo Shabalala <mfundoshabalala@gmail.com>
2022-04-30 19:19:58 +0200

    refactoring button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,10 +1,3 @@
-type ButtonInterface = React.FunctionComponent<{
-	name: string;
-	type?: 'button' | 'submit';
-	color?: string | 'primary' | 'danger' | 'disabled' | 'caution';
-	onClick?: (() => Promise<void>) | ((event: React.FormEvent) => void);
-}>;
-
 const Button: ButtonInterface = ({ name, type = 'button', color = 'primary' }) => {
 	return (
 		<button type={type} className={`px-6 py-2 rounded shadow max-w-fit ${color}`}>
--- interface/index.d.ts
+++ interface/index.d.ts
@@ -1,3 +1,10 @@
 type ChildrenInterface = React.FunctionComponent<{
 	children: React.ReactNode;
 }>;
+
+type ButtonInterface = React.FunctionComponent<{
+	name: string;
+	type?: 'button' | 'submit';
+	color?: string | 'primary' | 'danger' | 'disabled' | 'caution';
+	onClick?: (() => Promise<void>) | ((event: React.FormEvent) => void);
+}>;
